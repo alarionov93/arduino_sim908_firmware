51add987d8841642a69bd2f53e1ce8000db93165
Alex Larionov <alarionov93@yandex.ru>
2017-04-22 23:53:42 +0500

    trying to cast sms idx to integer

--- reading_str_ex/reading_str_ex.ino
+++ reading_str_ex/reading_str_ex.ino
@@ -4,16 +4,16 @@ int8_t answer = 0;
 int i = 0;
 
 char * pch;
-uint8_t sms_idx_str[3]="";
-int sms_idx = 0;
-uint8_t sms_from[15]="";
-// uint8_t sms_mode[10]="";
+char sms_idx_str[3]="";
+long sms_idx = 0;
+char sms_from[15]="";
+char sms_text[15]="";
 
 void setup() {
 	Serial.begin(19200);
 }
 void loop() {
-	char answ[300] = "+CMGL: 1,\"REC UNREAD\",\"+79655766572\",\"\",\"17/04/22,00:38:19+20\"\r\nGL\r\nOK";
+	char answ[300] = "+CMGL: 3,\"REC UNREAD\",\"+79655766572\",\"\",\"17/04/22,00:38:19+20\"\r\nGL\r\nOK";
 	if (strstr(answ, "+CMGL") != NULL) 
 	{
 		char data[15] = "";
@@ -25,19 +25,22 @@ void loop() {
 		int x = 0;
 		while( pch != NULL ) 
 		{
-		  memset(data, '\0', 20);	
+		  memset(data, '\0', 15);	
 		  sprintf(data, " %s\n", pch);
 		  // Serial.println(data);
 		  switch (x) 
 		  {
-		  	case 1:
-		  		sms_idx = atoi(data);
-		  		break;
+        case 1:
+          Serial.println(pch);
+          sms_idx = atoi(pch);
+          break;
 		  	case 3:
-		  		sms_from = data;
+//          Serial.println(data);
+		  		strcpy(sms_from, data);
 		  		break;
 		  	case 6:
-		  		sms_text = data;
+//          Serial.println(data);
+		  		strcpy(sms_text,data);
 		  		break;
 		  }
 		  // Serial.println(pch);
@@ -45,8 +48,8 @@ void loop() {
 		  x++;
 		}
 		Serial.println(sms_idx);
-		Serial.println(sms_from);
-		Serial.println(sms_text);
+//		Serial.println(sms_from);
+//		Serial.println(sms_text);
 
 		// pch = strtok(answ, ":");
   //   	Serial.print(pch);
